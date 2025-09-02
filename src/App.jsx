43bc156db9d4f2cdb7cbd376965221adae81bd30
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-09-02 11:08:56 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 
+// export default function App() {
+//   return (
+//      <div>
+//       <div class="grid grid-cols-2 max-sm:grid-cols-1">
+//   <div>01 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi optio iste voluptatum rem itaque impedit, libero, quas nihil magnam consequuntur aut reiciendis provident cum inventore cupiditate. Optio eveniet sint quidem?</div>
+
+//  <div>02 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi optio iste voluptatum rem itaque impedit, libero, quas nihil magnam consequuntur aut reiciendis provident cum inventore cupiditate. Optio eveniet sint quidem?</div>
+
+//  <div>03 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi optio iste voluptatum rem itaque impedit, libero, quas nihil magnam consequuntur aut reiciendis provident cum inventore cupiditate. Optio eveniet sint quidem?</div>
+
+//  <div>04 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi optio iste voluptatum rem itaque impedit, libero, quas nihil magnam consequuntur aut reiciendis provident cum inventore cupiditate. Optio eveniet sint quidem?</div>
+//  <div>05 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi optio iste voluptatum rem itaque impedit, libero, quas nihil magnam consequuntur aut reiciendis provident cum inventore cupiditate. Optio eveniet sint quidem?</div>
+//  <div>06 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi optio iste voluptatum rem itaque impedit, libero, quas nihil magnam consequuntur aut reiciendis provident cum inventore cupiditate. Optio eveniet sint quidem?</div>
+//  <div>07 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi optio iste voluptatum rem itaque impedit, libero, quas nihil magnam consequuntur aut reiciendis provident cum inventore cupiditate. Optio eveniet sint quidem?</div>
+//  <div>08 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi optio iste voluptatum rem itaque impedit, libero, quas nihil magnam consequuntur aut reiciendis provident cum inventore cupiditate. Optio eveniet sint quidem?</div>
+// </div>
+//      </div>
+//   )
+// }
+
+
 export default function App() {
   return (
-    <div>
-    
+    <div className="flex flex-wrap max-sm:flex-col">
+      <div className="w-1/2 max-sm:w-full max-sm:order-1">01</div>
+      <div className="w-1/2 max-sm:w-full max-sm:order-5">02</div>
+      <div className="w-1/2 max-sm:w-full max-sm:order-2">03</div>
+      <div className="w-1/2 max-sm:w-full max-sm:order-6">04</div>
+      <div className="w-1/2 max-sm:w-full max-sm:order-3">05</div>
+      <div className="w-1/2 max-sm:w-full max-sm:order-7">06</div>
+      <div className="w-1/2 max-sm:w-full max-sm:order-4">07</div>
+      <div className="w-1/2 max-sm:w-full max-sm:order-8">08</div>
     </div>
-  )
+  );
 }
