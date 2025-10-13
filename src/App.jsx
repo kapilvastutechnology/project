aa5ec5b02db1e687819f4640af8c8e777b5f251f
kapilvastutechnology <kapilvastutechnology81@gmail.com>
2025-10-13 11:44:22 +0545

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
-import { Button, Input } from "@heroui/react";
-import { useState } from "react";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@heroui/react";
 
 export default function App() {
-  const [data, setData] = useState([]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-  }
   return (
-    <div className="p-5 max-w-[300px]">
-
-
-      <form onSubmit={handleSubmit} className="space-y-4">
-
-        <Input
-          onChange={handleSubmit}
-          label='email' name="email" />
-
-        <Button type="submit">Submit</Button>
-      </form>
-
-    </div>
-  )
+    <Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="heroui logo"
+          height={40}
+          radius="sm"
+          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">HeroUI</p>
+          <p className="text-small text-default-500">heroui.com</p>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <p>Make beautiful websites regardless of your design experience.</p>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Link isExternal showAnchorIcon href="https://github.com/heroui-inc/heroui">
+          Visit source code on GitHub.
+        </Link>
+      </CardFooter>
+    </Card>
+  );
 }
